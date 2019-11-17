25c864474f5de79ec7206b2376812f590f07154b
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-17 18:19:39 +0000

    Automatically backed up by Learn

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -8,8 +8,8 @@ Spaceship.defaultProps = {
   render() {
     return (
       <div>
-        <h1>{this.props.name}</h1>
-        <h3>
+        <h1>Name: {this.props.name}</h1>
+        <p>Speed: {this.props.speed}</p>
       </div>
       )
   }
